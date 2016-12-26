1ce4d5e1db03696519ee14fe5c328d5850d216ee
Thomas Laroche <tho.laroche@gmail.com>
2016-12-26 17:46:09 +0100

    Build libjpeg versions 6, 7 and 8

--- build.py
+++ build.py
@@ -1,6 +1,14 @@
 from conan.packager import ConanMultiPackager
+import copy
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="libjpeg-turbo:shared", pure_c=True)
+
+    builds = []
+    for build in builder.builds:
+        for version in [6, 7, 8]:
+            build[1]["libjpeg-turbo:libjpeg_version"] = version
+            builds.append(copy.deepcopy(build))
+    builder.builds = builds
     builder.run()
